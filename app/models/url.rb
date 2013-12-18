92698de22bbcba9087c57cb4fb4f93d6664de375
Thuong Ho <thuong.t.ho@gmail.com>
2013-12-18 13:30:29 -0800

    add validate presence

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,5 +3,7 @@ class Url < ActiveRecord::Base
 
   # Write code the shows a url belongs to a student
   belongs_to :student
+  validates :url, :presence => true
   # Write code the validates urls have a domain name of herokuapp.com.
+  # validates :url, :format => { :with =>}
 end
