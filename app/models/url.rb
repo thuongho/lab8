5ea3f929b7e8079d208964b196cca3bdca0ff389
Thuong Ho <thuong.t.ho@gmail.com>
2013-12-18 14:16:15 -0800

    add regex validate to url

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,7 +3,6 @@ class Url < ActiveRecord::Base
 
   # Write code the shows a url belongs to a student
   belongs_to :student
-  validates :url, :presence => true
   # Write code the validates urls have a domain name of herokuapp.com.
-  # validates :url, :format => { :with => /@herokuapp\.com/}
+  validates :url, presence: true, format: { with: /\A[\w+\-.]+\.herokuapp\.com\z/}
 end
