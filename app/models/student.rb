09e0cf92560ddc194fe45ba30c2c0dd0db561fe4
Thuong Ho <thuong.t.ho@gmail.com>
2013-12-18 12:49:58 -0800

    validate students from list student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,10 @@ class Student < ActiveRecord::Base
   attr_accessible :course, :name
 
   # Write code allows a student to have many urls
+  has_many :urls
   # validate that :name is in the list of allowed students
+  validates :name, inclusion: { in: %w(dputnam adesilv1 ryaryy rgorreap ubenesch zng1 jswanner npachec2 avalenc7 bmckeand mpernabe dott bernst tcoffey1 gpaz ywun mweaver3 jsteele pkassian psakhark jsotobus bkrantzi crosales jmencha1 asushon kchan131 mhankins jmonto10 jreed13 ikim5 jpayne ktieu kposey1 dlouie11 zbazarra tho36 tparas amanayan jgarc166),
+  	message: "%{value} is not a student of this class"}
   # validate that :course is correct 
-
+  # validates :course, inclusion { in: %w(),}
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,5 +2,6 @@ class Url < ActiveRecord::Base
   attr_accessible :comment, :name, :student_id, :url
 
   # Write code the shows a url belongs to a student
+  belongs_to :student
   # Write code the validates urls have a domain name of herokuapp.com.
 end
